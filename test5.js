59f6ee4faffedd082e2a595840a91ff8d9f16d6f
Sean O'Connor <sean.oconnor@morningstar.com>
2015-09-02 13:07:01 -0500

    added simple bar chart

--- test5.js
+++ test5.js
@@ -1,18 +1,55 @@
 
-var YearData = Backbone.Model.extend();
+var YearData = Backbone.Model.extend({
+	initialize: function() {
+		this.bind("sync", this.syncItem, this);	
+	},
+	url: "testdata.json",
+	syncItem: function() {
+		//console.log(this.get("data"));
+	}
+});
 
+var ChartView = Backbone.View.extend({
+	initialize: function() {
+		this.model.bind("sync", this.render, this);
+	},
+	render: function() {
+		var yearData = this.model.toJSON().data;
+		var values = _.map(yearData, function(item) { return item.value; });
+		console.log(values);
+		
+		var x = d3.scale.linear()
+			.domain([0, d3.max(values)])
+			.range([0, 420]);
+
+		// clear element content
+		this.$el.html("");
+		
+		d3.select(".chart")
+		.selectAll("div")
+			.data(values)
+		.enter().append("div")
+			.style("width", function(d) { return x(d) + "px"; })
+			.text(function(d) { return d; });
+		
+		return this;
+	}
+});
+
+
+/*
 var ChartData = Backbone.Collection.extend({
+	initialize: function() {
+		this.bind("reset", this.resetCollection, this);	
+	},
 	model: YearData,
-	url: 'testdata.json'
+	url: "testdata.json",
+	resetCollection: function() {
+		console.log("Changed collection");
+	}
 });
+*/
 
-var data = new ChartData();
-
-data.fetch({
-  success:function() {
-    console.log(data.toJSON(),data.length);
-	console.log(data.models);
-  }
-});
-
-
+var yearData = new YearData();
+var chartView = new ChartView({el: ".chart", model: yearData});
+yearData.fetch();
